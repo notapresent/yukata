64386b7e9852d667f46a882335f15f00abd19ac3
notapresent <notapresent@gmail.com>
2015-05-03 23:29:20 +0000

    Add 1 minute schedule

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from google.appengine.ext import ndb
 
 SCHEDULES = {
+    '1m': 'Onca a minute',
     '15m': 'Every 15 minutes',
     '1h': 'Every hour',
     '1d': 'Once a day',
