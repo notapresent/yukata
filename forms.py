675d729106d25913180a1ae949d19645047f07b7
notapresent <notapresent@gmail.com>
2015-05-05 19:31:57 +0000

    remove debug nfo from account form, add validator

--- forms.py
+++ forms.py
@@ -5,9 +5,13 @@ from models.account import Account
 
 
 class AccountForm(model_form(Account, exclude=['apikey'])):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(AccountForm, self).__init__(*args, **kwargs)
+        # FIXME: Is this correct way of doing it?
+        self.display_name.validators = [validators.Length(min=3)]
 
 
 class AccountRegisterForm(AccountForm):
     accept_tos = BooleanField('I accept terms and conditions',
                               [validators.DataRequired()])
+
