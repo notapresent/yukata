e77d3e13565e81202f2e85b7773ea0c7c33bcda5
notapresent <notapresent@gmail.com>
2015-05-17 15:01:39 +0300

    Remove obsolete test

--- tests/test_backend_handlers.py
+++ tests/test_backend_handlers.py
@@ -27,12 +27,3 @@ class CronHandlerTestCase(unittest.TestCase):
 
     def uri_for(self, route_name, *args, **kwargs):
         return webapp2.uri_for(route_name, None, *args, **kwargs)
-
-    @patch('backend.handlers.TaskManager.enqueue_scheduled')
-    def test_cron_runrobots_calls_enque(self, mock_enqueue_scheduled):
-        schedule = SCHEDULES.keys()[0]
-        uri = self.uri_for('cron-run-scheduled-robots', schedule)
-        runrobot_uri = self.uri_for('task-runrobot')
-        response = self.app.get_response(uri)
-        self.assertEqual(200, response.status_int)
-        mock_enqueue_scheduled.assert_called_once_with(schedule, runrobot_uri)
